22c8538f5baf39a70463e0ac0ef109c1bd901b08
Juan Torres Reyes <juandtorres23@yahoo.com>
2022-07-19 22:00:34 -0400

    Finished LuckyNumbers

--- findLuckyNumbers.js
+++ findLuckyNumbers.js
@@ -1,2 +1,16 @@
 // Write your code below this line.
 // initial commit
+
+
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+let randomLuckyNumber = num => {
+    let randomNumber = []
+    for (let i = 0; i < num; i++) {
+        randomNumber.push(Math.floor(Math.random() * 11));
+     } 
+     return randomNumber
+ }
+console.log(randomLuckyNumber(6))
+
+
